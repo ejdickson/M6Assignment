f204f19e22821312bc81d5a36ec0e677fd22f8b6
Emma Brennan <ejdickson93@gmail.com>
2024-02-18 18:53:44 -0500

    Initial Commit

---
+++ M6Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M6Assignment
+//
+//  Created by Emma Brennan on 2/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
